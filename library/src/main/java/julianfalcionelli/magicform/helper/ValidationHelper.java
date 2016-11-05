0aa0d7ffa0dc6a9655db83658a131e6eb2bb41a2
nitin <nitin@dopplerlabs.com>
2016-11-05 14:40:37 -0700

    Adding new helper logic

--- library/src/main/java/julianfalcionelli/magicform/helper/ValidationHelper.java
+++ library/src/main/java/julianfalcionelli/magicform/helper/ValidationHelper.java
@@ -5,62 +5,57 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class ValidationHelper
-{
-	public static final String TAG = ValidationHelper.class.getSimpleName();
+import julianfalcionelli.magicform.R;
 
-	public static void cleanError(View view)
-	{
-		if (view instanceof EditText)
-		{
-			cleanError((EditText) view);
-			return;
-		}
+public class ValidationHelper {
+    public static final String TAG = ValidationHelper.class.getSimpleName();
 
-		if (view instanceof TextView)
-		{
-			((TextView) view).setError(null);
-			return;
-		}
+    public static void wrapIfNeeded(View viewToWrap){
 
-	}
+    }
 
-	private static void cleanError(EditText editText)
-	{
-		try
-		{
-			((TextInputLayout) editText.getParent()).setError(null);
-			((TextInputLayout) editText.getParent()).setErrorEnabled(false);
-		} catch (ClassCastException e)
-		{
-			editText.setError(null);
-		}
-	}
+    public static void cleanError(View view) {
+        if (view instanceof EditText) {
+            cleanError((EditText) view);
+            return;
+        }
 
-	public static void setError(View view, String error)
-	{
-		if (view instanceof EditText)
-		{
-			setError((EditText) view, error);
-			return;
-		}
+        if (view instanceof TextView) {
+            ((TextView) view).setError(null);
+            return;
+        }
 
-		if (view instanceof TextView)
-		{
-			((TextView) view).setError(error);
-			return;
-		}
-	}
+    }
 
-	private static void setError(EditText editText, String error)
-	{
-		try
-		{
-			((TextInputLayout) editText.getParent()).setErrorEnabled(true);
-			((TextInputLayout) editText.getParent()).setError(error);
-		} catch (ClassCastException e)
-		{
-			editText.setError(error);
-		}
-	}
+    private static void cleanError(EditText editText) {
+        try {
+            ((TextInputLayout) editText.getParent()).setError(null);
+            ((TextInputLayout) editText.getParent()).setErrorEnabled(false);
+        } catch (ClassCastException e) {
+            editText.setError(null);
+        }
+    }
+
+    public static void setError(View view, String error) {
+        if (view instanceof EditText) {
+            setError((EditText) view, error);
+            return;
+        }
+
+        if (view instanceof TextView) {
+            ((TextView) view).setError(error);
+            return;
+        }
+    }
+
+    private static void setError(EditText editText, String error) {
+        try {
+            TextInputLayout til = ((TextInputLayout) editText.getParent());
+            til.setHintTextAppearance(R.style.TILInput);
+            til.setErrorEnabled(true);
+            til.setError(error);
+        } catch (ClassCastException e) {
+            editText.setError(error);
+        }
+    }
 }
